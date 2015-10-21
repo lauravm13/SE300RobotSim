508d68ad9a23167cf0a9a425e8a458b4f2af8282
dequartd <dequartd@COB125-11.erau.edu>
2015-10-21 16:39:40 -0400

    added algorithim for class

--- Room.java
+++ Room.java
@@ -12,7 +12,9 @@ public class Room {
 	public Room(){
 		
 		//Add borders/dimensions to each room
-		public COB125;
+		public COB125; // make all these methods,each method will modify the path algorthims array, such as 0 would be the wall and the actual building,
+		// while 1's would be a position the robot could travel, and 2's,3's,4's could be the actaul obstacles that the robot could not move to. and the 
+		// robot would be the value 5. 
 		public Atrium;
 		public Hallway;
 	}
