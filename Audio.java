df0866a2801b233772ac26b485d9b5168758a44c
theish <theish@LB370-04.erau.edu>
2015-11-08 17:37:44 -0500

    just tinkering around :)

--- Audio.java
+++ Audio.java
@@ -1,4 +1,11 @@
+import java.io.File;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineListener;
 
 /**
  * @author mayorl
@@ -9,17 +16,37 @@
 public class Audio {
 
 	public Audio(){
+		// specify the sound to play
+		// (assuming the sound can be played by the audio system)
+		File soundFile = new File(   FileDialog dialog = new FileDialog(shell, SWT.OPEN);
+		dialog.setFilterExtensions(new String [] {"*.html"});
+		dialog.setFilterPath("c:\\temp");
+		String result = dialog.open(););
 
-	}
+		AudioInputStream sound = AudioSystem.getAudioInputStream(soundFile);
 
-	public void finalize() throws Throwable {
+		// load the sound into memory (a Clip)
+		DataLine.Info info = new DataLine.Info(Clip.class, sound.getFormat());
+		Clip clip = (Clip) AudioSystem.getLine(info);
+		clip.open(sound);
 
-	}
-	/**
-	 * @param Play
-	 * @return false
-	 */
-	public boolean Play(){
-		return false;
-	}
-}//end Audio
+		// due to bug in Java Sound, explicitly exit the VM when
+		// the sound has stopped.
+		clip.addLineListener(new LineListener() {
+			public void update(LineEvent event) {
+				if (event.getType() == LineEvent.Type.STOP) {
+					event.getLine().close();
+					System.exit(0);
+				}
+
+				public void finalize() throws Throwable {
+
+				}
+				/**
+				 * @param Play
+				 * @return false
+				 */
+				public boolean Play(){
+					return false;
+				}
+			}//end Audio
