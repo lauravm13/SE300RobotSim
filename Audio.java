6f856ee9bd9dd2f1499189f9c1316c67ec2cb8d5
theish <theish@LB164-11.erau.edu>
2015-11-10 19:15:02 -0500

    started to create needed text files for audio messages

--- Audio.java
+++ Audio.java
@@ -1,3 +1,4 @@
+import java.io.PrintWriter;
 import java.io.File;
 
 import javax.sound.sampled.AudioInputStream;
@@ -12,32 +13,42 @@ import javax.sound.sampled.LineListener;
  * @version 1.0
  * @created 07-Oct-2015 5:06:18 PM
  * Description: Audio file that will play messages about the different majors
+ * code adapted from :http://www.java2s.com/Code/Java/Development-Class/AnexampleofloadingandplayingasoundusingaClip.htm 
+ * and https://wiki.eclipse.org/FAQ_How_do_I_prompt_the_user_to_select_a_file_or_a_directory%3F
+ * 
  */
 public class Audio {
 
 	public Audio(){
+		//create files for each program 
+		//electrical computer software and systems engineering
+		String message1 = "Message one ";
+		PrintWriter.outputStream = new PrintWriter(message1);
+		
+		
+	}
 		// specify the sound to play
 		// (assuming the sound can be played by the audio system)
-		File soundFile = new File(   FileDialog dialog = new FileDialog(shell, SWT.OPEN);
-		dialog.setFilterExtensions(new String [] {"*.html"});
-		dialog.setFilterPath("c:\\temp");
-		String result = dialog.open(););
+		//File soundFile = new File{  FileDialog dialog = new FileDialog(shell, SWT.OPEN);
+		//dialog.setFilterExtensions(new String [] {"*.mp3"});
+		//dialog.setFilterPath("c:\\temp");
+		//String result = dialog.open();}
 
-		AudioInputStream sound = AudioSystem.getAudioInputStream(soundFile);
+		//AudioInputStream sound = AudioSystem.getAudioInputStream(soundFile);
 
 		// load the sound into memory (a Clip)
-		DataLine.Info info = new DataLine.Info(Clip.class, sound.getFormat());
-		Clip clip = (Clip) AudioSystem.getLine(info);
-		clip.open(sound);
+		//DataLine.Info info = new DataLine.Info(Clip.class, sound.getFormat());
+		//Clip clip = (Clip) AudioSystem.getLine(info);
+		//clip.open(sound);
 
 		// due to bug in Java Sound, explicitly exit the VM when
 		// the sound has stopped.
-		clip.addLineListener(new LineListener() {
-			public void update(LineEvent event) {
-				if (event.getType() == LineEvent.Type.STOP) {
-					event.getLine().close();
-					System.exit(0);
-				}
+		//clip.addLineListener(new LineListener() {
+			//public void update(LineEvent event) {
+				//if (event.getType() == LineEvent.Type.STOP) {
+					//event.getLine().close();
+					//System.exit(0);
+				//}
 
 				public void finalize() throws Throwable {
 
